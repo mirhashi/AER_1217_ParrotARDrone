fix bug in vel calculation

position_controller.py
@@ -22,25 +22,25 @@ class PositionController(object):
 		self.Kp_x = 1
 		self.Kp_y = 1
 		self.Kv_x = 1
-		self.Kx_y = 1
+		self.Kv_y = 1
 		self.g = -9.81
 		self.z_acc = 0
 
 		self.x_i = 0
-		self.t_i = 0 
+		self.t_i = -1
 		self.y_i = 0
 
 		self.xdes_i = 0
-		self.tdes_i = 0 
+		self.tdes_i = -1
 		self.ydes_i = 0
 
    
     def actual_vel_calc(self,X,Y,t):
 		X_dot = (X - self.x_i)/(t - self.t_i)
 		Y_dot = (Y - self.y_i)/(t - self.t_i)
-		self.x_i = X;
-		self.y_i = Y;
-		self.t_i = t; 
+		self.x_i = X
+		self.y_i = Y
+		self.t_i = t 
 		return X_dot,Y_dot
 
     def desired_vel_calc(self,X,Y,t):
@@ -59,9 +59,9 @@ class PositionController(object):
 		x_acc = self.Kv_x*(xdes_dot - x_dot) + self.Kp_x*(xdes - x)
 		y_acc = self.Kv_y*(ydes_dot - y_dot) + self.Kp_y*(ydes - y)
 
-		f = (self.z_acc + self.g)/(cos(theta)*cos(phi))
+		f = (self.z_acc + self.g)/(np.cos(theta)*np.cos(phi))
 
-		phi_c = np.asin(-y_acc/f)
-		theta_c = np.asin(-x_acc/(f*cos(phi_c))) 
+		phi_c = np.arcsin(-y_acc/f)
+		theta_c = np.arcsin(-x_acc/(f*np.cos(phi_c))) 
 
 		return phi_c, theta_c